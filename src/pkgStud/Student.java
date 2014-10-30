70640a666bddd32f989c1def7b316a271b7383f3
Ajit Singh <Ajit.Singh@Scitegrity.com>
2014-10-30 11:39:41 +0000

    Commit:30/10/14 devB-no.03

--- src/pkgStud/Student.java
+++ src/pkgStud/Student.java
@@ -1,5 +1,6 @@
 package pkgStud;
 
+/** Student class. */
 public class Student extends PersonalRecord {
 
  private String name;
@@ -7,7 +8,10 @@ public class Student extends PersonalRecord {
  private double marks;
  
  Student(String studName, int studAge, double studMarks, String studAddr) {
-  super(studAddr); // pass Address to Personal Record class.
+
+  // pass Address to Personal Record class.
+  super(studAddr);
+
   this.name= studName;
   this.age= studAge;
   this.marks= studMarks;
